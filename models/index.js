44cf68f00201811657092ceb44d1ef57b24dc348
Jairo-Jr <jairo.huallpa1@gmail.com>
2022-10-18 12:25:27 -0500

    Nuevo modelo wish

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const ENGINE_DB = process.env.ENGINE_DB;
 const pathModels = (ENGINE_DB === 'nosql') ? './nosql' : './mysql';
-
+// Agregado el nuevo modelo - wish
 const models = {
     usersModel: require(`${pathModels}/user`),
     tracksModel: require(`${pathModels}/tracks`),
